272b257663e828d3408f9e73fb5b0de29f1e8771
ahmedSadoun <ahmedsaadoun866@gmail.com>
2022-09-04 22:32:55 +0200

    NavLink and Link routes

--- reactrouter/src/App.js
+++ reactrouter/src/App.js
@@ -1,9 +1,35 @@
-import './App.css';
-
+import "./App.css";
+import { Fragment } from "react";
+import Home from "./pages/Home.js";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import { BrowserRouter,NavLink, Route,Routes } from "react-router-dom";
+// BrowserRouter is going to wrap our entire application that contain routes and this is something that we are going to use to make a route
+// Link is built with special functionality , when we click on one of these links react doesn't actually go to the server to request the page it interrupts request and stops it , then inject the component we need to the blank html page
+//the difference between the NavLink and Link is that The NavLink is used when you want to highlight a link as active we give it our own styles
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <nav>
+          <h1>Me Articles </h1>
+         <NavLink to="/">Home</NavLink>
+         <NavLink to="/about">About</NavLink>
+         <NavLink to="/contact">Contact</NavLink>
+        </nav>
+       
+      <Fragment>
+        {/* <Navbar/> */}
+        <Routes>
+            <Route exact path='/' element={<Home/>}/>
+
+          <Route exact path='/contact' element={<Contact/>}/>
+          <Route exact path='/about' element={<About/>}/>
+        </Routes>
+      </Fragment>
+   
      
+      </BrowserRouter>
     </div>
   );
 }
